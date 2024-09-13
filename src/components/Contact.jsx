9896ce5126f641942b323c322abf69e4726cb930
Muhammad Yasin <m.yaseensalim@gmail.com>
2024-09-13 23:29:45 +0500

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
   });
 
   const [loading, setLoading] = useState(false);
-
+  
   const handleChange = (e) => {
     const { target } = e;
     const { name, value } = target;
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Muhammad Yasin",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "m.yaseensalim@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
